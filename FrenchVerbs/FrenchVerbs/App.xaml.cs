e91f6a3e348807ac6cd9e59655d8f283c5717610
Leopik <leopik3@gmail.com>
2019-10-26 20:57:50 +0300

    init

---
+++ FrenchVerbs/FrenchVerbs/App.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FrenchVerbs
+{
+    public partial class App : Application
+    {
+
+        public static string VerbsDBPath { get; private set; }
+        public App()
+        {
+            InitializeComponent();
+
+            var embeddedResourceDb = Assembly.GetExecutingAssembly().GetManifestResourceNames().First(s => s.Contains("verbs.db"));
+            var embeddedResourceDbStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(embeddedResourceDb);
+
+
+            VerbsDBPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "verbs.db");
+            // Check if your DB has already been extracted.
+            if (!File.Exists(VerbsDBPath) | true)
+            {
+                using (BinaryReader br = new BinaryReader(embeddedResourceDbStream))
+                {
+                    using (BinaryWriter bw = new BinaryWriter(new FileStream(VerbsDBPath, FileMode.Create)))
+                    {
+                        byte[] buffer = new byte[2048];
+                        int len = 0;
+                        while ((len = br.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            bw.Write(buffer, 0, len);
+                        }
+                    }
+                }
+            }
+
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ FrenchVerbs/FrenchVerbs/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace FrenchVerbs
+{
+
+    public class VerbForms
+    {
+        [PrimaryKey]
+        public string Word { get; set; }
+        public string First_Singular { get; set; }
+        public string Second_Singular { get; set; }
+        public string Third_Singular { get; set; }
+        public string First_Plural { get; set; }
+        public string Second_Plural { get; set; }
+        public string Third_Plural { get; set; }
+    }
+
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private List<VerbForms> verbs;
+        private int currWordIndx = 0;
+        private SQLiteAsyncConnection dbConn;
+        private VerbForms currWord;
+        public VerbForms CurrentWord { get
+            {
+                return currWord;
+            } 
+            set
+            {
+                currWord = value;
+                Console.WriteLine("asdasdasdasd asd     "+currWordIndx);
+                infinitive.Text = currWord.Word;
+                first_sng.TextColor = Color.Gray;
+                first_sng.Text = "";
+                second_sng.TextColor = Color.Gray;
+                second_sng.Text = "";
+                third_sng.TextColor = Color.Gray;
+                third_sng.Text = "";
+                first_plr.TextColor = Color.Gray;
+                first_plr.Text = "";
+                second_plr.TextColor = Color.Gray;
+                second_plr.Text = "";
+                third_plr.TextColor = Color.Gray;
+                third_plr.Text = "";
+            }
+        }
+        
+        public MainPage()
+        {
+            InitializeComponent();
+
+            dbConn = new SQLiteAsyncConnection(App.VerbsDBPath);
+            
+            next_btn.Clicked += (a, b) => CurrentWord = verbs[++currWordIndx];
+            chk_btn.Clicked += (a, b) => CheckCorrectness();
+            answr_btn.Clicked += (a, b) => ShowAnswer();
+
+            dbConn.QueryAsync<VerbForms>("select * from present_indicative order by random()").ContinueWith((queryTsk) =>
+            {
+                verbs = queryTsk.Result;
+                next_btn.IsEnabled = true;
+                answr_btn.IsEnabled = true;
+                chk_btn.IsEnabled = true;
+
+                CurrentWord = verbs[currWordIndx];
+            });
+        }
+
+        private void ShowAnswer()
+        {
+            first_sng.Text = CurrentWord.First_Singular;
+            second_sng.Text = CurrentWord.Second_Singular;
+            third_sng.Text = CurrentWord.Third_Singular;
+            first_plr.Text = CurrentWord.First_Plural;
+            second_plr.Text = CurrentWord.Second_Plural;
+            third_plr.Text = CurrentWord.Third_Plural;
+            CheckCorrectness();
+        }
+
+        private void CheckCorrectness()
+        {
+            first_sng.TextColor = first_sng.Text == currWord.First_Singular ? Color.Green : Color.Red;
+            second_sng.TextColor = second_sng.Text == currWord.Second_Singular ? Color.Green : Color.Red;
+            third_sng.TextColor = third_sng.Text == currWord.Third_Singular ? Color.Green : Color.Red;
+            first_plr.TextColor = first_plr.Text == currWord.First_Plural ? Color.Green : Color.Red;
+            second_plr.TextColor = second_plr.Text == currWord.Second_Plural ? Color.Green : Color.Red;
+            third_plr.TextColor = third_plr.Text == currWord.Third_Plural ? Color.Green : Color.Red;
+        }
+    }
+}
